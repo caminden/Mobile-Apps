1ac2738755754f1d2178b5351f717789104ddef4
chase minden <caminden@gmail.com>
2020-08-24 14:27:31 -0500

    Initialize repo

---
+++ android/app/src/main/kotlin/edu/uco/cminden/map20/lesson1/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.cminden.map20.lesson1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Lesson1App());
+}
+
+class Lesson1App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Done: Implement Build
+    return MaterialApp(
+      home: Text("My first app"),
+    );
+  }
+
+}
