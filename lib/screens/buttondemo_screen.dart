71250a4c04ded86c1451eba6078542efc3e8a6cf
chase minden <caminden@gmail.com>
2020-08-25 20:28:05 -0500

    Final commit part 1

--- lib/screens/buttondemo_screen.dart
+++ lib/screens/buttondemo_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:lesson1/screens/imagedemo_screen.dart';
 
 class ButtonDemoScreen extends StatelessWidget {
   static const routeName = '/buttonDemoScreen';
+  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('Button Demo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('Floating Action Button is pressed'),
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red[200],
       ),
       body: Column(children: <Widget>[
         RaisedButton(
@@ -26,7 +31,19 @@ class ButtonDemoScreen extends StatelessWidget {
           onPressed: () => print('Raised Button 2 is pressed'),
           icon: Icon(Icons.book, color: Colors.red),
           label: Text('Raised Button 2'),
-          
+        ),
+        FlatButton(
+          onPressed: () => print('Flat Button is pressed'),
+          child: Text(
+            'Flat Button',
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+          color: Colors.teal[900],
+          splashColor: Colors.teal[100],
+        ),
+        IconButton(
+          icon: Icon(Icons.broken_image, size: 70.0, color: Colors.blue),
+          onPressed: () => print('Icon Button is pressed'),
         )
       ]),
     );
